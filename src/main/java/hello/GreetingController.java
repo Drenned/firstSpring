b99dfe8af5b6db51520093b98457cf6dd052edde
Drenned <drenned0207@gmail.com>
2019-03-31 03:36:11 +0500

    Переделал контроллер

--- src/main/java/hello/GreetingController.java
+++ src/main/java/hello/GreetingController.java
@@ -1,19 +1,48 @@
 package hello;
 
+import hello.repository.UserRepository;
+import hello.user.User;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 
+import java.util.List;
 import java.util.Map;
 
 @Controller
 public class GreetingController {
 
+    @Autowired
+    UserRepository userRepository;
+
     @GetMapping("/greeting")
     public String greeting(@RequestParam(name="name", required=false, defaultValue="World") String name, Map<String, Object> map) {
         map.put("name", name);
         return "greeting";
+    }
+
+    @GetMapping("/add")
+    public String addUser(@RequestParam String name,
+                                        @RequestParam String email,
+                                        Map<String, Object> model)
+    {
+        User user = new User();
+        user.setName(name);
+        user.setEmail(email);
+        userRepository.save(user);
+        model.put("user", user);
+        return "Saved";
+    }
+
+    @GetMapping("/all")
+    public
+    String getAllUser(Map<String,Object> model)
+    {
+        Iterable<User> users=userRepository.findAll();
+        model.put("users",users);
+        return "all";
     }
 
     @GetMapping
